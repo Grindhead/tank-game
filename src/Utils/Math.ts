521783aa60eef1455a8a87323e4d26c90ffad486
Craig Beswetherick <craigbeswetherick@gmail.com>
2023-11-01 06:06:06 +0000

    added a clamp method to utils

--- src/Utils/Math.ts
+++ src/Utils/Math.ts
@@ -8,3 +8,13 @@
 export const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+/**
+ * Helper function to clamp a value between a minimum and maximum
+ * @param value - the number to clamp
+ * @param min - the minimum value
+ * @param max - the maximum value
+ */
+export const clamp = (value: number, min: number, max: number): number => {
+  return Math.max(min, Math.min(value, max));
+};
