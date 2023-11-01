3cf0b985ae24d93a7513adc86867e2b894731098
Craig Beswetherick <craigbeswetherick@gmail.com>
2023-11-01 06:05:54 +0000

    updaded moveSpriteTowardsMouse method

--- src/Utils/moveSpriteTowardsMouse.ts
+++ src/Utils/moveSpriteTowardsMouse.ts
@@ -1,5 +1,6 @@
 import { Point, Sprite } from 'pixi.js';
 import { mousePosition } from './getMousePosition';
+
 /**
  * an array of all the sprites
  */
@@ -14,29 +15,22 @@ export const addMoveSpriteTowardsMouse = (sprite: Sprite): void => {
   spriteList?.push(sprite);
 };
 
-/**
- * moves all the sprites added towards the mouse
- * @param timeDelta - the current time delta
- * @param speed - the speed to move the sprite
- */
 export const updateSpriteMovement = (
   timeDelta: number,
-  speed: number
+  speed: number,
+  friction: number,
+  collisionTargets: Sprite[][]
 ): void => {
-  spriteList?.forEach((sprite: Sprite) => {
+  spriteList?.forEach((playerSprite: Sprite) => {
     // Calculate the direction vector to the mouse position
-
-    const pos = sprite.getGlobalPosition();
-
+    const pos = playerSprite.getGlobalPosition();
     const direction = new Point(
       mousePosition.x - pos.x,
       mousePosition.y - pos.y
     );
 
     // Calculate the length of the direction vector (distance to the mouse)
-    const distance = Math.sqrt(
-      direction.x * direction.x + direction.y * direction.y
-    );
+    const distance = Math.sqrt(direction.x ** 2 + direction.y ** 2);
 
     if (distance > 5) {
       // Normalize the direction vector
@@ -46,9 +40,47 @@ export const updateSpriteMovement = (
       // Calculate the amount to move in this frame
       const moveAmount = speed * (timeDelta / 1000);
 
+      // Calculate the potential new position
+      let newX = playerSprite.x + direction.x * moveAmount;
+      let newY = playerSprite.y + direction.y * moveAmount;
+
+      // Check for collisions with all specified collision targets
+      let isColliding = false;
+
+      for (const targetSprites of collisionTargets) {
+        for (const targetSprite of targetSprites) {
+          const playerBounds = playerSprite.getBounds();
+          const targetBounds = targetSprite.getBounds();
+
+          if (playerBounds.intersects(targetBounds)) {
+            isColliding = true;
+            break; // Exit the inner loop on collision
+          }
+        }
+        if (isColliding) break; // Exit the outer loop on collision
+      }
+
+      // Apply friction when colliding
+      if (isColliding) {
+        // Calculate the normalized collision normal vector
+        const normalX = (newX - playerSprite.x) / moveAmount;
+        const normalY = (newY - playerSprite.y) / moveAmount;
+
+        // Calculate the dot product of the movement direction and collision normal
+        const dotProduct = direction.x * normalX + direction.y * normalY;
+
+        // Apply friction to the movement direction
+        direction.x -= normalX * dotProduct * friction;
+        direction.y -= normalY * dotProduct * friction;
+
+        // Recalculate the new position after friction is applied
+        newX = playerSprite.x + direction.x * moveAmount;
+        newY = playerSprite.y + direction.y * moveAmount;
+      }
+
       // Update the player's position
-      sprite.x += direction.x * moveAmount;
-      sprite.y += direction.y * moveAmount;
+      playerSprite.x = newX;
+      playerSprite.y = newY;
     }
   });
 };
